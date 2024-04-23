2f8405ac7c39b61bce8d35943ecc18bbfc6adda5
SektorCyrax <140064073+SektorCyrax@users.noreply.github.com>
2024-04-23 12:24:49 +0100

    Update one_page_website.js

--- One-Page Website/one_page_website.js
+++ One-Page Website/one_page_website.js
@@ -1,225 +1,79 @@
-<body>
-  <style>
-    html {
-      box-sizing: border-box;
-    }
-    
-    *, *:before, *:after {
-      box-sizing: inherit;
-    }
-    
-    body {
-      margin: 0;
-    }
-    
-    .preview {
-      width: 100%;
-    }
-    
-    .row {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-    }
-    
-    .row > .col {
-      padding: 0 8px;
-    }
-    
-    .col {
-      float: left;
-      width: 25%;
-    }
-    
-    .modal {
-      display: none;
-      position: fixed;
-      z-index: 1;
-      padding: 10px 62px 0px 62px;
-      left: 0;
-      top: 0;
-      width: 100%;
-      height: 100%;
-      overflow: auto;
-      background-color: black;
-    }
-    
-    .modal-content {
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      margin: auto;
-      padding: 0 0 0 0;
-      width: 80%;
-      max-width: 1200px;
-    }
-    
-    .slide {
-      display: none;
-    }
-    
-    .image-slide {
-    	width: 100%;
-    }
-    
-    .modal-preview {
-    	width: 100%;
-    }
-    
-    .dots {
-    	display: flex;
-    	flex-direction: row;
-    	justify-content: space-between;
-    }
-    
-    img.preview, img.modal-preview {
-      opacity: 0.6;
-    }
-    
-    img.active,
-    .preview:hover,
-    .modal-preview:hover {
-      opacity: 1;
-    }
-    
-    img.hover-shadow {
-      transition: 0.3s;
-    }
-    
-    .hover-shadow:hover {
-      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-    } 
-    
-    .close {
-      color: white;
-      position: absolute;
-      top: 10px;
-      right: 25px;
-      font-size: 35px;
-      font-weight: bold;
-    }
-    
-    .close:hover,
-    .close:focus {
-      color: #999;
-      text-decoration: none;
-      cursor: pointer;
-    }
-    
-    .previous,
-    .next {
-      cursor: pointer;
-      position: absolute;
-      top: 50%;
-      width: auto;
-      padding: 16px;
-      margin-top: -50px;
-      color: white;
-      font-weight: bold;
-      font-size: 20px;
-      transition: 0.6s ease;
-      border-radius: 0 3px 3px 0;
-      user-select: none;
-      -webkit-user-select: none;
-    }
-    
-    .next {
-      right: 0;
-      border-radius: 3px 0 0 3px;
-    }
-    
-    .previous:hover,
-    .next:hover {
-      background-color: rgba(0, 0, 0, 0.8);
-    }
-  </style>
-  <div class="row">
-    <div class="col">
-       <img src="https://static.pexels.com/photos/385997/pexels-photo-385997.jpeg" onclick="openLightbox();toSlide(1)" class="hover-shadow preview" alt="Toy car on the road." />
-    </div>
-    <div class="col">
-       <img src="https://static.pexels.com/photos/574521/pexels-photo-574521.jpeg" onclick="openLightbox();toSlide(2)" class="hover-shadow preview" alt="Toy car exploring offroad." />
-    </div>
-    <div class="col">
-       <img src="https://static.pexels.com/photos/386009/pexels-photo-386009.jpeg" onclick="openLightbox();toSlide(3)" class="hover-shadow preview" alt="Toy car in the city" />
-    </div>
-  </div>
-  
-  <div id="Lightbox" class="modal">
-  
-    <span class="close pointer" onclick="closeLightbox()">&times;</span>
-    <div class="modal-content">
-      <div class="slide">
-        <img src="https://static.pexels.com/photos/385997/pexels-photo-385997.jpeg" class="image-slide" alt="Toy car on the road." />
-      </div>
-      <div class="slide">
-        <img src="https://static.pexels.com/photos/574521/pexels-photo-574521.jpeg" class="image-slide" alt="Toy car exploring offroad." />
-      </div>    
-      <div class="slide">
-        <img src="https://static.pexels.com/photos/386009/pexels-photo-386009.jpeg" class="image-slide" alt="Toy car in the city." />
-      </div>
-    
-      <a class="previous" onclick="changeSlide(-1)">&#10094;</a>
-      <a class="next" onclick="changeSlide(1)">&#10095;</a>
-          
-      <div class="dots">
-        <div class="col">
-          <img src="https://static.pexels.com/photos/385997/pexels-photo-385997.jpeg" class="modal-preview hover-shadow" onclick="toSlide(1)" alt="Toy car on the road." />
-        </div>
-        <div class="col">
-          <img src="https://static.pexels.com/photos/574521/pexels-photo-574521.jpeg" class="modal-preview hover-shadow" onclick="toSlide(2)" alt="Toy car exploring offroad." />
-        </div>
-        <div class="col">
-          <img src="https://static.pexels.com/photos/386009/pexels-photo-386009.jpeg" class="modal-preview hover-shadow" onclick="toSlide(3)" alt="Toy car in the city" />
-        </div>
-      </div>
-    </div> 
-  </div>
-  
-  <script>
-    let slideIndex = 1;
-    showSlide(slideIndex);
-    
-    function openLightbox() {
-      document.getElementById('Lightbox').style.display = 'block';
-    };
-    
-    function closeLightbox() {
-      document.getElementById('Lightbox').style.display = 'none';
-    };
-    
-    function changeSlide(n) {
-      showSlide(slideIndex += n);
-    };
-    
-    function toSlide(n) {
-      showSlide(slideIndex = n);
-    };
-    
-    function showSlide(n) {
-      const slides = document.getElementsByClassName('slide');
-      let modalPreviews = document.getElementsByClassName('modal-preview');
-    
-      if (n > slides.length) {
-        slideIndex = 1;	
-      };
-      
-      if (n < 1) {
-        slideIndex = slides.length;
-      };
-    
-      for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      };
-      
-      for (let i = 0; i < modalPreviews.length; i++) {
-        modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
-      };
-      
-      slides[slideIndex - 1].style.display = 'block';
-      modalPreviews[slideIndex - 1].className += ' active';
-    };
-  </script>
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+	<style>
+	body {background-color: gray;}
+	h1   {color: gold;}
+	p    {color: white;}
+	</style>
+		<title>Armor Wars</title>
+		<meta charset = "utf-8">
+	</head>
+	<body>
+		<header>
+			<h1><em><center>Armor Wars</em></h1>
+		</header>
+			<p><a href = "#Home" id = link">Home
+			</a></p>
+			<p><a href = "#Armors" id >Armors
+			</a></p>
+			<p><a href = "#Sign-In">Sign-In
+			</a></p>
+			<div id = "Home">
+				<div>
+					<ul>
+						<p>Welcome to Armor Wars, the first and last word in body armor history.</p>
+						<p>Here you will find details and images of every kind of armor created to protect warriors past and present, alongside prototypes that may one day guard the lives of soliders in the future.</p>
+					</ul>
+				</div>
+			</div>
+		<section>
+			<div id = "Armors">
+				<h1><em><center>A small selection of the armors available to view</em></h1>
+			<div>
+				<div>
+					<img src = "./images/thumbs/thumbnail_1.jpg"
+						width = "300"
+						height = "500" />
+					<img src = "./images/thumbs/thumbnail_2.jpg"
+						width = "300"
+						height = "500" />	
+					<img src = "./images/thumbs/thumbnail_3.jpg"
+						width = "300"
+						height = "500" />
+					<img src = "./images/thumbs/thumbnail_4.jpg"
+						width = "300"
+						height = "500" />
+					<img src = "./images/thumbs/thumbnail_5.jpg"
+						width = "400"
+						height = "500" />
+				</div>
+			</div>
+		</section>
+		<section>
+			<div>
+				<h1><em><center>How to forge medieval armor</em></h1>
+			<div>
+				<div>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/NsNPNxq7re0?si=lWw059rxGKGXilHb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+				</div>
+			</div>	
+		</section>
+		<section>
+			<div id ="Sign-In">
+				<div>
+				<footer>
+				<form action = "" method = "get">
+				<p>Are you a member?  Sign in now!</p>
+				First name: <input type = "text" name = "First Name"><br>
+				Last name: <input type = "text" name = "Last Name"><br>
+				Email: <input type = "email" name = "Email"><br>
+				Password: <input type = "password" name = "Password"><br>
+				<input type = "submit" value = "Submit">
+				</form>
+				</footer>
+				</div>
+			</div>	
 </body>
+</html>
 
